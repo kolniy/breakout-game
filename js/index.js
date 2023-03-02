808d45825f4cfa951d3d9b9343ad73f6e626912f
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-02 11:01:33 +0100

    added game start login

--- js/index.js
+++ js/index.js
@@ -11,8 +11,17 @@ let bricks;
 let newBrick;
 let brickInfo;
 
+let playing = false;
+let startButton;
+
+const textStyle = { font: "18px Arial", fill: "#0095DD" };
+
 let scoreText;
 let score = 0;
+
+let lives = 3;
+let livesText;
+let lifeLostText;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -23,6 +32,7 @@ function preload() {
   game.load.image("ball", "img/ball.png");
   game.load.image("paddle", "img/paddle.png");
   game.load.image("brick", "img/brick.png");
+  game.load.spritesheet("ball", "img/wobble.png", 20, 20);
 }
 
 function create() {
@@ -32,6 +42,7 @@ function create() {
     game.world.height - 25,
     "ball"
   );
+  ball.animations.add("wobble", [0, 1, 0, 2, 0, 1, 0, 2, 0], 24);
   ball.anchor.set(0.5);
   paddle = game.add.sprite(
     game.world.width * 0.5,
@@ -42,29 +53,55 @@ function create() {
 
   game.physics.enable(ball, Phaser.Physics.ARCADE);
 
-  ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
   game.physics.enable(paddle, Phaser.Physics.ARCADE);
   paddle.body.immovable = true;
   game.physics.arcade.checkCollision.down = false;
   ball.checkWorldBounds = true;
-  ball.events.onOutOfBounds.add(() => {
-    alert("Game over!");
-    location.reload();
-  }, this);
+  ball.events.onOutOfBounds.add(ballLeaveScreen, this);
   initBricks();
 
-  scoreText = game.add.text(5, 5, "Points: 0", {
-    font: "18px Arial",
-    fill: "#0095DD",
-  });
+  // score text login
+  scoreText = game.add.text(5, 5, "Points: 0", textStyle);
+
+  // lives text logic
+  livesText = game.add.text(
+    game.world.width - 5,
+    5,
+    `Lives: ${lives}`,
+    textStyle
+  );
+  livesText.anchor.set(1, 0);
+  lifeLostText = game.add.text(
+    game.world.width * 0.5,
+    game.world.height * 0.5,
+    "Life lost, click to continue",
+    textStyle
+  );
+  lifeLostText.anchor.set(0.5);
+  lifeLostText.visible = false;
+  game.load.spritesheet("button", "img/button.png", 120, 40);
+
+  startButton = game.add.button(
+    game.world.width * 0.5,
+    game.world.height * 0.5,
+    "button",
+    startGame,
+    this,
+    1,
+    0,
+    2
+  );
+  startButton.anchor.set(0.5);
 }
 
 function update() {
-  game.physics.arcade.collide(ball, paddle);
+  game.physics.arcade.collide(ball, paddle, ballHitPaddle);
   game.physics.arcade.collide(ball, bricks, ballHitBrick);
-  paddle.x = game.input.x || game.world.width * 0.5;
+  if (playing) {
+    paddle.x = game.input.x || game.world.width * 0.5;
+  }
 }
 
 function initBricks() {
@@ -100,12 +137,18 @@ function initBricks() {
 }
 
 function ballHitBrick(ball, brick) {
-  brick.kill();
+  const killTween = game.add.tween(brick.scale);
+  killTween.to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.None);
+  killTween.onComplete.addOnce(() => {
+    brick.kill();
+  }, this);
+  killTween.start();
+
   score += 10;
   scoreText.setText(`Points: ${score}`);
 
   let count_alive = 0;
-  for (let i = 0; i < bricks.childern.length; i++) {
+  for (let i = 0; i < bricks.children.length; i++) {
     if (bricks.children[i].alive) {
       count_alive++;
     }
@@ -114,4 +157,31 @@ function ballHitBrick(ball, brick) {
     alert("You won the game, congratulations!");
     location.reload();
   }
+}
+
+function ballLeaveScreen() {
+  lives--;
+  if (lives) {
+    livesText.setText(`Lives: ${lives}`);
+    lifeLostText.visible = true;
+    ball.reset(game.world.width * 0.5, game.world.height - 25);
+    paddle.reset(game.world.width * 0.5, game.world.height - 5);
+    game.input.onDown.addOnce(() => {
+      lifeLostText.visible = false;
+      ball.body.velocity.set(150, -150);
+    }, this);
+  } else {
+    alert("You lost, game over!");
+    location.reload();
+  }
+}
+
+function ballHitPaddle(ball, paddle) {
+  ball.animations.play("wobble");
+}
+
+function startGame() {
+  startButton.destroy();
+  ball.body.velocity.set(150, -150);
+  playing = true;
 }
