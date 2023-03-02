7eb65b3b7bf48fc0c370d108b4237258704821ec
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-02 09:36:28 +0100

    added collision dectection between game and ball

--- js/index.js
+++ js/index.js
@@ -5,6 +5,14 @@ const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 });
 
 let ball;
+let paddle;
+
+let bricks;
+let newBrick;
+let brickInfo;
+
+let scoreText;
+let score = 0;
 
 function preload() {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -13,15 +21,84 @@ function preload() {
   game.stage.backgroundColor = "#eee";
 
   game.load.image("ball", "img/ball.png");
+  game.load.image("paddle", "img/paddle.png");
+  game.load.image("brick", "img/brick.png");
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(50, 50, "ball");
+  ball = game.add.sprite(
+    game.world.width * 0.5,
+    game.world.height - 25,
+    "ball"
+  );
+  ball.anchor.set(0.5);
+  paddle = game.add.sprite(
+    game.world.width * 0.5,
+    game.world.height - 5,
+    "paddle"
+  );
+  paddle.anchor.set(0.5, 1);
+
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  ball.body.velocity.set(150, 150);
+
+  ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true;
+  game.physics.arcade.checkCollision.down = false;
+  ball.checkWorldBounds = true;
+  ball.events.onOutOfBounds.add(() => {
+    alert("Game over!");
+    location.reload();
+  }, this);
+  initBricks();
+
+  scoreText = game.add.text(5, 5, "Points: 0", {
+    font: "18px Arial",
+    fill: "#0095DD",
+  });
 }
 
-function update() {}
+function update() {
+  game.physics.arcade.collide(ball, paddle);
+  game.physics.arcade.collide(ball, bricks, ballHitBrick);
+  paddle.x = game.input.x || game.world.width * 0.5;
+}
+
+function initBricks() {
+  brickInfo = {
+    width: 50,
+    height: 20,
+    count: {
+      row: 3,
+      col: 7,
+    },
+    offset: {
+      top: 50,
+      left: 60,
+    },
+    padding: 10,
+  };
+  bricks = game.add.group();
+  for (let c = 0; c < brickInfo.count.col; c++) {
+    for (let r = 0; r < brickInfo.count.row; r++) {
+      // create new brick and add it to the group
+      const brickX =
+        c * (brickInfo.width + brickInfo.padding) + brickInfo.offset.left;
+      const brickY =
+        r * (brickInfo.height + brickInfo.padding) + brickInfo.offset.top;
+
+      newBrick = game.add.sprite(brickX, brickY, "brick");
+      game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+      newBrick.body.immovable = true;
+      newBrick.anchor.set(0.5);
+      bricks.add(newBrick);
+    }
+  }
+}
+
+function ballHitBrick(ball, brick) {
+  brick.kill();
+}
